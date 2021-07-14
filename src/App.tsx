51566db30a388b8fe778914b922526cc1dc7ad16
Jacob Runo <jacobkip254@gmail.com>
2021-07-14 05:52:51 -0700

    image folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { encode } from 'blurhash';
+import { Blurhash } from 'react-blurhash';
+import image from './Image/black.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <canvas    >
+
+      </canvas>
+      {/* <Blurhash
+        hash=""
+        width={400}
+        height={300}
+        resolutionX={32}
+        resolutionY={32}
+      /> */}
+
     </div>
   );
 }
